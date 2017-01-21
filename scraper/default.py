ba0af1f4c2b1998eb7d869bedb33991dc9d6aaa8
simonjpartridge <me@simonjpartridge.co.uk>
2017-01-21 02:54:53 -0800

    added lib

--- scraper/default.py
+++ scraper/default.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from newspaper import article
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
@@ -9,7 +12,18 @@ app.config['DEBUG'] = True
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
+
+
     return 'Hello World!'
+
+
+@app.route('proc')
+def processArticle:
+    art = article(url="http://www.huffingtonpost.com/entry/trump-cut-mortgage-insurance_us_5882765ee4b0e3a73568f0a4")
+
+    art.download()
+    art.parse()
+    print(art.text)
 
 
 @app.errorhandler(404)
