4fdea7cc92158dfb7fc5d6e1f0a11a99dc33703b
simonjpartridge <me@simonjpartridge.co.uk>
2017-01-21 02:58:13 -0800

    added lib

--- scraper/default.py
+++ scraper/default.py
@@ -19,11 +19,12 @@ def hello():
 
 @app.route('/proc')
 def processArticle():
-    art = article(url="http://www.huffingtonpost.com/entry/trump-cut-mortgage-insurance_us_5882765ee4b0e3a73568f0a4")
-
-    art.download()
-    art.parse()
-    print(art.text)
+    return "fvidfvmdfmv"
+    # art = article(url="http://www.huffingtonpost.com/entry/trump-cut-mortgage-insurance_us_5882765ee4b0e3a73568f0a4")
+    #
+    # art.download()
+    # art.parse()
+    # print(art.text)
 
 
 @app.errorhandler(404)
