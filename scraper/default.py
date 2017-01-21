18e26b0db53456569352d0a83e3491f5acb10c3f
simonjpartridge <me@simonjpartridge.co.uk>
2017-01-21 02:56:02 -0800

    added lib

--- scraper/default.py
+++ scraper/default.py
@@ -17,8 +17,8 @@ def hello():
     return 'Hello World!'
 
 
-@app.route('proc')
-def processArticle:
+@app.route('/proc')
+def processArticle():
     art = article(url="http://www.huffingtonpost.com/entry/trump-cut-mortgage-insurance_us_5882765ee4b0e3a73568f0a4")
 
     art.download()
