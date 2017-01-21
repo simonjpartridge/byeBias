343b4d81d8c73385e222430567011b984e077de2
simonjpartridge <me@simonjpartridge.co.uk>
2017-01-21 13:35:25 -0800

    back to main.app

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+from scraper import default
+
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
@@ -10,6 +12,16 @@ app.config['DEBUG'] = True
 def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
+
+
+@app.route('/proc')
+def lala():
+    return "proc"
+
+
+@app.route('/defa')
+def fghrghfh():
+    return default.return_yo()
 
 
 @app.errorhandler(404)
